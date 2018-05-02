b352ee1724eea15f5cdb1833211d4ae140e4fa61
Gr3enP0ison <wewantthejoker@gmail.com>
2018-05-02 09:49:20 +0200

    Small Update

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # exa-node
-Beispielbeschreibung
+=========
+
+A small module to interface the database Exasol using JDBC
+
+## Installation
+
+  `npm install exa-node`
+
+## Requirements
+    * JDK (currently only x86 supported)
+    * NodeJS
+    * Exasol-DB-Instance
+
+## Usage
+
+    var numFormatter = require('@jdaudier/number-formatter');
+
+    var formattedNum = numFormatter(35666);
+  
+  
+  Output should be `35,666`
+
+
+## Tests
+
+  `npm test`
+
+## Contributing
+
+In lieu of a formal style guide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code.
--- test/test.js
+++ test/test.js
@@ -1,46 +1,68 @@
 'use strict';
 
 var expect = require('chai').expect;
+var ExaNode = require('../exa-node.js');
 var numFormatter = require('../index');
 
-describe('#numFormatter', function() {
-    it('should convert single digits', function() {
+describe('#numFormatter', function () {
+    it('should convert single digits', function () {
         var result = numFormatter(1);
         expect(result).to.equal('1');
     });
 
-    it('should convert double digits', function() {
+    it('should convert double digits', function () {
         var result = numFormatter(12);
         expect(result).to.equal('12');
     });
 
-    it('should convert triple digits', function() {
+    it('should convert triple digits', function () {
         var result = numFormatter(123);
         expect(result).to.equal('123');
     });
 
-    it('should convert 4 digits', function() {
+    it('should convert 4 digits', function () {
         var result = numFormatter(1234);
         expect(result).to.equal('1,234');
     });
 
-    it('should convert 5 digits', function() {
+    it('should convert 5 digits', function () {
         var result = numFormatter(12345);
         expect(result).to.equal('12,345');
     });
 
-    it('should convert 6 digits', function() {
+    it('should convert 6 digits', function () {
         var result = numFormatter(123456);
         expect(result).to.equal('123,456');
     });
 
-    it('should convert 7 digits', function() {
+    it('should convert 7 digits', function () {
         var result = numFormatter(1234567);
         expect(result).to.equal('1,234,567');
     });
 
-    it('should convert 8 digits', function() {
+    it('should convert 8 digits', function () {
         var result = numFormatter(12345678);
         expect(result).to.equal('12,345,678');
     });
+
+    it('tests db connection', function (done) {
+        ExaNode.getAllUsers(function (err, results) {
+            if (err) {
+                console.log('Scotty, we have an error!!');
+                return;
+            }
+            if (results != null) {
+
+                expect(results[0].USER_NAME).to.equal('SYS');
+                done();
+
+
+            }
+
+        });
+        //expect(results[0].USER_NAME).to.equal('SYS');
+
+    });
+
+
 });
